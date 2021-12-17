22f5153462f5a207bb995ba28fc179a31faadb50
weltkind6 <yaroslavzef@icloud.com>
2021-12-17 19:03:02 +0300

    select with checkboxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Option from "./UI/Option";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+            <h1>Hotel yo</h1>
+            <Option/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import makeAnimated from "react-select/animated";
+import {components} from "react-select";
+import {colourOptions} from "./UI/data";
+import MySelect from "./UI/MySelect";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Option = props => {
+    return (
+        <div>
+            <components.Option {...props}>
+                <input
+                    type="checkbox"
+                    checked={props.isSelected}
+                    onChange={() => null}
+                />{" "}
+                <label>{props.label}</label>
+            </components.Option>
+        </div>
+    );
+};
+
+const MultiValue = props => (
+    <components.MultiValue {...props}>
+        <span>{props.data.label}</span>
+    </components.MultiValue>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const animatedComponents = makeAnimated();
+
+export default class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            optionSelected: null
+        };
+    }
+
+    handleChange = selected => {
+        this.setState({
+            optionSelected: selected
+        });
+    };
+
+    render() {
+        return (
+            <MySelect
+                options={colourOptions}
+                isMulti
+                closeMenuOnSelect={false}
+                hideSelectedOptions={false}
+                components={{Option, MultiValue, animatedComponents}}
+                onChange={this.handleChange}
+                allowSelectAll={true}
+                value={this.state.optionSelected}
+            />
+        );
+    }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Example/>, rootElement);
+
 reportWebVitals();
